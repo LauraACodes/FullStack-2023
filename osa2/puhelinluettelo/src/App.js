9ce8ab5569f23e83125491d72932a9c6549d68b1
Laura Andstrom <laura.andstrom@helsinki.fi>
2023-08-22 13:21:36 +0300

    Done ->2.15

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import FilterForm from './components/FilterForm.js'
 import PersonForm from './components/PersonForm.js'
 import PersonList from './components/PersonList.js'
-import axios from 'axios'
+import personService from './services/persons.js'
 
 const App = () => {
   const [persons, setPersons] = useState([]) 
@@ -12,14 +12,14 @@ const App = () => {
   const [newFilter, setNewFilter] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
+    personService
+      .getAll()
+      .then(initialPersons => {
         console.log('promise fulfilled')
-        setPersons(response.data)
+        setPersons(initialPersons)
       })
   }, [])
-  
+
   const handlePersonNameChange = (event) => {
     console.log('filling name ', event.target.value)
     setNewName(event.target.value)
@@ -38,18 +38,44 @@ const App = () => {
   const addPerson = (event) => {
     event.preventDefault()
     console.log('clicked', event.target)
+
     if (persons.findIndex((person) => person.name === newName) == -1) {
       const personObject = {
         name: newName,
         number: newNumber
       }
-      setPersons(persons.concat(personObject))
-      setNewName('')
-      setNewNumber('')
+      personService
+        .create(personObject)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
+          setNewName('')
+          setNewNumber('')
+        })
     }
     else {
       console.log('tupla')
-      alert(`${newName} is already added to phonebook`)
+      if (window.confirm(`${newName} is already added to phonebook. Replace the old number with a new one?`)) {
+        const person = persons.find(p => p.name === newName)
+        const updatedPerson = { ...person, number: newNumber}
+
+        personService
+          .updateNumber(person.id, updatedPerson)
+          .then(updatedP=> {
+            setPersons(persons.map(p => p.id !== person.id ? p : updatedP))
+            setNewName('')
+            setNewNumber('')
+          })
+      }
+    }
+  }
+
+  const deletePerson = (id) => {
+    console.log('deleteä kutsuttiin')
+
+    if (window.confirm(`Sure to delete ${persons.find( (person) => person.id === id).name}?`)) {
+      personService
+        .deletePerson(id)
+        .then(() => setPersons(persons.filter(person => person.id !== id)))
     }
   }
 
@@ -60,7 +86,7 @@ const App = () => {
       <h2>Add a new</h2>
       <PersonForm addPerson={addPerson} newName={newName} handlePersonNameChange={handlePersonNameChange} newNumber={newNumber} handlePersonNumberChange={handlePersonNumberChange} />
       <h2>Numbers</h2>
-      <PersonList persons={persons} newFilter={newFilter} />
+      <PersonList persons={persons} newFilter={newFilter} deletePerson={deletePerson}/>
     </div>
   )
 
--- osa2/puhelinluettelo/src/components/PersonList.js
+++ osa2/puhelinluettelo/src/components/PersonList.js
@@ -1,10 +1,10 @@
 import Person from "./Person"
 
-const PersonList = ( {persons, newFilter} ) => {
+const PersonList = ( {persons, newFilter, deletePerson} ) => {
     return (
         <div>
         {persons.filter((person) => person.name.toLowerCase().includes(newFilter.toLowerCase()))
-        .map(person => <Person key={person.name} person={person} />
+        .map(person => <Person key={person.id} person={person} deletePerson={() => deletePerson(person.id)}/>
         )}
         </div>
     )
